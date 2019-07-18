b9190d47bd32eaf6bcca4ebb6d4fb66dd43a5f13
riccardo soro <riccardosoro95@gmail.com>
2019-07-18 16:34:08 +0200

    second job

--- BD-DirectorActorJob/src/main/java/it/unibo/bd1819/job2/reducers/CounterRatingReducer.java
+++ BD-DirectorActorJob/src/main/java/it/unibo/bd1819/job2/reducers/CounterRatingReducer.java
@@ -1,5 +1,7 @@
 package it.unibo.bd1819.job2.reducers;
 
+import it.unibo.bd1819.job2.utils.Utility;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.log4j.Logger;
@@ -12,23 +14,28 @@ public class CounterRatingReducer extends Reducer<Text, Text, Text, Text> {
 
     /**
      * Reduce tasks that compute the average rating of each book
+     *
      * @param key
      * @param values
      * @param context
      * @throws IOException
      * @throws InterruptedException
      */
-    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         Double rating = 0.0;
-        int count = 0;
-        for(Text r : values){
-            rating += Double.parseDouble(r.toString());
-            count ++;
+
+        int countBM = 0;
+        int countRating = 0;
+        for (IntWritable r : values) {
+            if (r.equals(new IntWritable(Utility.IDENTIFIER))) {
+                countBM++;
+            } else {
+                rating += Double.parseDouble(r.toString());
+                countRating++;
+            }
         }
         double avg = 0;
-        if(count > 0)
-            avg = rating / count;
-
-        context.write(key, new Text(String.valueOf(avg)));
+        if (countRating > 0) avg = rating / countRating;
+        context.write(key, new Text(avg +"\t"+countBM));
     }
 }
--- BD-DirectorActorJob/src/main/java/it/unibo/bd1819/job2/utils/Utility.java
+++ BD-DirectorActorJob/src/main/java/it/unibo/bd1819/job2/utils/Utility.java
@@ -1,4 +1,5 @@
 package it.unibo.bd1819.job2.utils;
 
 public class Utility {
+    public static final int IDENTIFIER = 6;
 }
